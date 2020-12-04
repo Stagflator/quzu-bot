8d492580962b7c4de907271ac0e01788c2d170b0
Stagflator <40017736+Stagflator@users.noreply.github.com>
2020-12-04 23:42:10 +0400

    Updated the temporary text

--- app/main.py
+++ app/main.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Quzu bot will be here soon.....'
+    return 'I am Quzu bot that is created by Farid Jafarov for taking care for his quzu. I will be ready soon, I am under process right now. Love quzu <3'
